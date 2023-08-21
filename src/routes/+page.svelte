6090dde6247a51201dd7a595a115d224d2469d02
mthiagar <madhuran.v@gmail.com>
2023-08-20 21:42:12 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+
+<div class="full-height flex column items-center justify-center align-middle">
+<h1 class="text-3xl font-bold">
+    SYNBIO.DEV 
+  </h1>
+  
+  <h2 class="text-2xl font-bold">
+    Coming Soon
+  </h2>
+
+</div>
+
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+
+  </style>
