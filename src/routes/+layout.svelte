39ea6e139b0f34141423be5dc55f3f327e2e0854
mthiagar <madhuran.v@gmail.com>
2023-08-21 18:22:01 -0400

    basic page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
-    import "../app.css";
-  </script>
+  import Header from './Header.svelte';
+  import '../app.css';
+</script>
+
+<div class="app">
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <p> This be the footer</p>
+  </footer>
+</div>
   
-  <slot />
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+</style>
